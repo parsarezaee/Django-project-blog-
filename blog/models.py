ff2b36e021de3413c2bcfb004c8a197a3d3d372e
parsa-r7 <parsarezaee777@yahoo.com>
2021-10-16 15:43:46 +0330

    image model changes

--- blog/models.py
+++ blog/models.py
@@ -32,7 +32,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
